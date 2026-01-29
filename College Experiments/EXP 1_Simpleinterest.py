61c08869d54e9a3896e53d71873ae2ec92dbcec2
Jiya Singh <scholarjiyasingh@gmail.com>
2026-01-29 13:06:10 +0530

    Add files via upload

---
+++ College Experiments/EXP 1_Simpleinterest.py
@@ -0,0 +1,6 @@
+#SIMPLE INTEREST
+p=float(input("Enter principle amount:"))
+r=float(input("Enter rate:"))
+t=float(input("Enter time period in years:"))
+si=(p*r*t)/100
+print("Simple Interest is",si)
