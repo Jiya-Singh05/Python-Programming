0982d9eb024271c03ac9e7f93212a23153cf0558
Jiya Singh <scholarjiyasingh@gmail.com>
2026-01-29 13:15:49 +0530

    Add files via upload

---
+++ College Experiments/EXP _ Hypotenuse.py
@@ -0,0 +1,5 @@
+#Write a program to compute the hypotenuse of a right triangle using Pythagoras
+l=float(input("Enter height of the triangle:"))
+b=float(input("Enter base of the triangle:"))
+hyp=((l**2)+(b**2))**0.5
+print("Hypotenuse of the traingle is",hyp)
