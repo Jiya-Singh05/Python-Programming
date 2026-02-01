6bf67748ad33655c1386dfd9785333dc45254dc2
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-01 15:06:40 +0530

    Add files via upload

---
+++ College Experiments/EXP _Data types.py
@@ -0,0 +1,10 @@
+# Data types in Python
+integer= 10          
+floatno= 12.5       
+string= "Python"    
+boolean= True      
+
+print("Int value is:", integer)
+print("Float value is:", floatno)
+print("String value is:", string)
+print("Boolean value is:", boolean)
