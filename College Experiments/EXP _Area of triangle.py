a8357c9997f938012897cc2523f6911ac1403bc3
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-01 14:58:12 +0530

    Add files via upload

---
+++ College Experiments/EXP _Area of triangle.py
@@ -0,0 +1,6 @@
+#Write a program to find area of triangle when length of sides are given. 
+h=float(input("Enter height of the triangle:"))
+b=float(input("Enter base of the triangle:"))
+area=0.5*h*b
+print("Area of the triangle is:", area)
+
