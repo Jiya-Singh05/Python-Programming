d6d8eb9a053c0d33067fdabbc307dcf57ac1e546
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-01 15:03:43 +0530

    Add files via upload

---
+++ College Experiments/EXP _Swap number.py
@@ -0,0 +1,7 @@
+#Write a program to swap two numbers without taking additional variable
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+
+a,b=b,a
+
+print(f"Swapped value: first number={a} second number={b}")
