54605c4cb5e08e1c596fd49729a08c251e6295a4
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-02 09:14:19 +0530

    Add files via upload

---
+++ College Experiments/EXP _Greatest btw 2.py
@@ -0,0 +1,9 @@
+#Find the greatest among the two numbers. If numbers are equal than print “numbers are equal”.
+a=float(input("Enter your no.a:"))
+b=float(input("Enter your no.b:"))
+if(a>b):
+     print("a is greater")
+elif(b>a):
+     print("b is greater")
+else:
+     print("a and b are equal")
