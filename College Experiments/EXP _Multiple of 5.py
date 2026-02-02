97e3263651c519927cd19b11bb1dc45f823f16aa
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-02 09:14:49 +0530

    Add files via upload

---
+++ College Experiments/EXP _Multiple of 5.py
@@ -0,0 +1,8 @@
+#Check whether a given number is multiple of five or not.
+
+a=float(input("Enter your no.a:"))
+
+if(a%5==0):
+    print("YES, it is a multiple of 5")
+else:
+    print("NO, it is not a multiple of 5")
