74bdb5cc1dee421916f87b56bd8f0cfc6beefe2c
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-02 08:40:44 +0530

    Add files via upload

---
+++ College Experiments/EXP _Divisible by 3 and 5.py
@@ -0,0 +1,8 @@
+#Check whether the given number is divisible by 3 and 5 both
+n=float(input("Enter a number: "))
+
+if (n%3==0 and n%5==0):
+    print("YES, it is divisible by 3 and 5 both")
+else:
+    print("NO, it is not divisible by 3 and 5 both")
+    
